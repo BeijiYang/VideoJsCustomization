dee0de6bf561f4aaf00c56b65e7171007a85d91a
BeijiYang <langyuyang@outlook.com>
2017-10-15 20:51:31 +0800

    standardjs

--- src/containers/CourseContainer.js
+++ src/containers/CourseContainer.js
@@ -17,7 +17,7 @@ class CourseContainer extends Component {
       }],
       poster: 'http://videojs.com/img/logo.png',
       fluid: 'true', // put the player in the VideoPlayerWrap box
-      'playbackRates': [0.75, 1, 1.5, 2],
+      playbackRates: [0.75, 1, 1.5, 2],
       controlBar: {
         volumePanel: {
           inline: false // vertical VolumeControl
