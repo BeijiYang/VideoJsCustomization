84ca4d1ba205460f18d2f1d939c0dc94f19c9aa1
BeijiYang <langyuyang@outlook.com>
2017-10-16 10:46:50 +0800

    single src: mp4

--- src/containers/CourseContainer.js
+++ src/containers/CourseContainer.js
@@ -11,9 +11,6 @@ class CourseContainer extends Component {
       sources: [{
         src: 'http://vjs.zencdn.net/v/oceans.mp4',
         type: 'video/mp4'
-      }, {
-        src: 'http://vjs.zencdn.net/v/oceans.webm',
-        type: 'video/webm'
       }],
       poster: 'http://videojs.com/img/logo.png',
       fluid: 'true', // put the player in the VideoPlayerWrap box
